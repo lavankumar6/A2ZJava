42939787d0637a3aabe95298ca0e406c13c731c7
Laxman Kumar <lavan76@gmail.com>
2019-06-28 12:50:00 -0500

    Create ClassOne.java

---
+++ A2ZJava/src/org/lkg/java/ClassOne.java
@@ -0,0 +1,26 @@
+package org.lkg.java;
+
+public class ClassOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("Hello World");
+		//
+		/*
+		int t;
+		System.out.println(t);  -- This gives the compile time error, since In java , we have the intialize any local variable be start using them.
+		*/
+		
+		/* Here it throws the compile time error - Unresolved compilation problem: 
+			Type mismatch: cannot convert from int to boolean	
+			BEcause Zero is used in place of  boolean condition.( like i< 10)
+		 for(int i = 0; 0; i++)  
+	      {  
+	          System.out.println("Hello");  
+	          break;  
+	      }  
+	      
+	      */
+	}
+
+}
